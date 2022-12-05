5f46ad569dfe3fe36ac4e76b38660445619113ca
Abed Toumeh <a.toumeh2000@gmail.com>
2022-12-05 08:24:47 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,87 @@
+//jshint esversion:6
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+
+const app = express();
+
+//To try read the .env file and the secret stuff in it we can do this
+console.log(process.env.API_KEY);
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb://localhost:27017/userDB", {
+  useNewUrlParser: true,
+});
+//Database with encryption
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String,
+});
+// we took secret constant from here to .env file to keep it secret so instead of keep it like this we are going to change our code a little
+// const secret = "Thisisourlittlesecret.";
+
+//Changed one
+// userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
+//the new one
+userSchema.plugin(encrypt, {
+  secret: process.env.SECRET,
+  encryptedFields: ["password"],
+});
+
+//we should make a secret and plugin it with our database schema to make it work
+
+const User = new mongoose.model("User", userSchema);
+
+app.get("/", function (req, res) {
+  res.render("home");
+});
+
+app.get("/login", function (req, res) {
+  res.render("login");
+});
+
+app.get("/register", function (req, res) {
+  res.render("register");
+});
+
+app.post("/register", function (req, res) {
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password,
+  });
+
+  newUser.save(function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("secrets");
+    }
+  });
+});
+
+app.post("/login", function (req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({ email: username }, function (err, foundUser) {
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUser) {
+        if (foundUser.password === password) {
+          res.render("secrets");
+        }
+      }
+    }
+  });
+});
+
+app.listen(3000, function () {
+  console.log("Server started on port 3000.");
+});
